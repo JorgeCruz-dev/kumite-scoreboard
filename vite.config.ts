9428d7e78090c7fa86946463d5d301603c79e948
Rafa Cruz <jorgerafaelcruzflores@gmail.com>
2025-03-19 17:38:35 -0600

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/kumite-scoreboard'
+  base: 'https://JorgeCruz-dev.github.io/kumite-scoreboard'
 })
