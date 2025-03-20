ea5d399d80782ff7f46cd5ed0ef554f5aaf3606a
Rafa Cruz <jorgerafaelcruzflores@gmail.com>
2025-03-19 18:06:48 -0600

    modify scoreboard

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://JorgeCruz-dev.github.io/kumite-scoreboard'
+  base: '/kumite-scoreboard'
 })
